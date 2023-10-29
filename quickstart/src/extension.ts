144e6a37ca7bedd21391c2fa4cc8fcaf110d2c26
Yoichi Kawasaki <yokawasa@gmail.com>
2023-10-29 18:57:40 +0900

    update with the latest config and dependency packages
    Signed-off-by: Yoichi Kawasaki <yokawasa@gmail.com>

--- quickstart/src/extension.ts
+++ quickstart/src/extension.ts
@@ -2,18 +2,18 @@ import * as vscode from 'vscode';
 import { dateFormat } from "./dateformat";
 export function activate(context: vscode.ExtensionContext) {
 
-  let disposable_helloworld = vscode.commands.registerCommand('extension.helloWorld', () => {
+  let disposableHelloWorld = vscode.commands.registerCommand('quickstart.helloWorld', () => {
     vscode.window.showInformationMessage('Hello World!');
   });
   
-  let disposable_gettoday = vscode.commands.registerCommand('extension.getToday', () => {
+  let disposableGetToday = vscode.commands.registerCommand('quickstart.getToday', () => {
     let today: Date = new Date();
     vscode.window.showInformationMessage("Today:" + dateFormat(today));
   });
 
   context.subscriptions.push(
-    disposable_helloworld,
-    disposable_gettoday
+    disposableHelloWorld,
+    disposableGetToday
   );
 }
 
