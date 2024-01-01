9080a703c26185ea24c6fd52e9303e2aae7901b9
Yoichi Kawasaki <yokawasa@gmail.com>
2024-01-01 08:08:51 +0700

    Add deprecating code (only supported in rev1)
    Signed-off-by: Yoichi Kawasaki <yokawasa@gmail.com>

---
+++ mytranslator-webpack/src/extension.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+import { ApiClient } from './apiclient';
+import { Utilities } from "./utilities";
+
+let targetLanguageCode: string,
+    isVerified: boolean,
+    apiclient: ApiClient;
+
+function loadConfig(): boolean {
+
+  let {
+    subKeyTranslator,
+    targetLanguage,
+  } = vscode.workspace.getConfiguration("mytranslator");
+
+  targetLanguageCode = targetLanguage;
+  apiclient = new ApiClient( subKeyTranslator );
+
+  if (!Utilities.isSupportedlanguageCode(targetLanguageCode )) {
+    vscode.window.showErrorMessage(`MyTranslator Error: Language code is invalid or empty: ${targetLanguageCode}`);
+    isVerified = false;
+    return false;
+  }
+  isVerified = true;
+  return isVerified;
+}
+
+export function activate(context:vscode.ExtensionContext) {
+
+  // Load all configuration values in initial activation
+  loadConfig();
+
+  // Register command
+  let disposable_translate =  vscode.commands.registerCommand('extension.mytranslator.translate', async() => {
+    try {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+
+        if (!isVerified) {
+          throw new Error('Invalid Configuration!');
+        }
+        if (editor.selections.length > 1) {
+          throw new Error('Multiple text is not supported!');
+        }
+
+        const text = editor.document.getText(
+            new vscode.Range(
+              editor.selections[0].start,
+              editor.selections[0].end)
+          );
+        vscode.window.showInformationMessage(`Translating your text => Target Language (${targetLanguageCode})`);
+        apiclient.translate(text, targetLanguageCode);
+      }
+    } catch (e) {
+      vscode.window.showErrorMessage(`MyTranslator Error: ${e.message}`);
+    } 
+   });
+
+  // Trigger reloading config when the configuration changed.
+  let disposable_configchange =  vscode.workspace.onDidChangeConfiguration( ()=>loadConfig() );
+
+  context.subscriptions.push(
+    disposable_translate,
+    disposable_configchange
+  );
+}
+
+export function deactivate() {}
---
+++ mytranslator/src/apiclient.ts
@@ -0,0 +1,65 @@
+'use strict';
+import * as needle from 'needle';
+import * as vscode from 'vscode'; 
+import { Utilities } from "./utilities";
+const uuidv4 = require('uuid/v4');
+
+export class ApiClient {
+  private outputChannel: vscode.OutputChannel;
+  private TRANSLATE_API_ENDPOINT="https://api.cognitive.microsofttranslator.com/translate";
+  private subKeyTranslator: string;
+
+  constructor(subKeyTranslator: string) {
+    this.outputChannel = vscode.window.createOutputChannel('MyTranslator');
+    this.subKeyTranslator = subKeyTranslator;
+  }
+
+  translate( text : string, targetlang: string = 'ja'){
+
+    this.translateInternal(text, targetlang)
+    .then((jsontext) => {
+      let item:any = jsontext[0];
+      let translated_text:string = item["translations"][0]["text"];
+
+      this.outputChannel.show();
+      this.outputChannel.appendLine(`Source: ${text}`);
+      this.outputChannel.appendLine(`Translation: ${translated_text}`);
+      this.outputChannel.appendLine('\n');
+    })
+    .catch((err: Error) => {
+      throw new Error(`Translation API request failed: ${err.message}`);
+    });
+  }
+
+  private translateInternal(text:string, targetlang:string = 'ja'): Promise<string> {
+
+    let options = {
+      headers: {
+        'Ocp-Apim-Subscription-Key': this.subKeyTranslator,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': uuidv4().toString()
+      }
+    };
+    let body =[{
+       'text': text
+    }];
+    let qs = Utilities.queryString(
+      {
+      'api-version': '3.0',
+      'to': [targetlang] 
+      }
+    );
+
+    return new Promise((resolve, reject) => {
+      needle.post( `${this.TRANSLATE_API_ENDPOINT}?${qs}`, body, options, function(err:any, res:any) {
+        if (err) {
+          return reject(err);
+        }
+        if (res.statusCode !== 200) {
+          return reject(new Error(`Wrong status code ${res.statusCode} in Azure Cognitive Translator API`));
+        }
+        resolve(res.body);
+      });
+    });
+  }
+}
